3de844985c773a268cc9fb72261c4b118a9cb9a5
Hannes Cho <hoonsungcho11@gmail.com>
2022-04-13 15:40:24 +0200

    hello

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import { Server } from "socket.io";
+import logger from "morgan";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,7 @@ const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -20,3 +22,7 @@ const handleListening = () =>
 const server = app.listen(PORT, handleListening);
 
 const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("helloGuys", () => console.log("the client said hello"));
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,6 @@
+io("/");
+const socket = io("/");
 
+socket.on("hello", () => console.log("Somebody joined"));
+
+setTimeout(() => socket.emit("helloGuys"), 4000);
