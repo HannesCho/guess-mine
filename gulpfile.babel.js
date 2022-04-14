d7a6ce1cb479d21db3e2e47fec1c94a834f8f7fb
Hannes Cho <hoonsungcho11@gmail.com>
2022-04-14 11:01:20 +0200

    gulp bro

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@ import gulpSass from "gulp-sass";
 import nodeSass from "node-sass";
 import autoprefixer from "gulp-autoprefixer";
 import minifyCSS from "gulp-csso";
+import del from "del";
+import bro from "gulp-bro";
+import babel from "babelify";
 
 const sass = gulpSass(nodeSass);
 
@@ -12,10 +15,17 @@ const paths = {
     dest: "src/static/styles",
     watch: "assets/scss/**/*.scss",
   },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js",
+    watch: "assets/js/**/*.js",
+  },
 };
 
-function styles() {
-  return gulp
+const clean = () => del(["src/static"]);
+
+const styles = () =>
+  gulp
     .src(paths.styles.src)
     .pipe(sass())
     .pipe(
@@ -25,12 +35,26 @@ function styles() {
     )
     .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
-}
 
-function watchFiles() {
+const js = () =>
+  gulp
+    .src(paths.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babel.configure({
+            presets: ["@babel/preset-env"],
+          }),
+        ],
+      })
+    )
+    .pipe(gulp.dest(paths.js.dest));
+
+const watchFiles = () => {
   gulp.watch(paths.styles.watch, styles);
-}
+  gulp.watch(paths.js.watch, js);
+};
 
-const dev = gulp.series([styles, watchFiles]);
+const dev = gulp.series(clean, styles, js, watchFiles);
 
 export default dev;
