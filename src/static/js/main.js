00752a06efeefb48c0ffadd347fd1d1d84a9b6b2
Hannes Cho <hoonsungcho11@gmail.com>
2022-04-20 08:24:02 +0200

    player one

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -40,7 +40,7 @@ if (sendMsg) {
   sendMsg.addEventListener("submit", handleSendMsg);
 }
 
-},{"./sockets":6}],2:[function(require,module,exports){
+},{"./sockets":7}],2:[function(require,module,exports){
 "use strict";
 
 var _sockets = require("./sockets");
@@ -81,7 +81,7 @@ if (loginForm) {
   loginForm.addEventListener("submit", handleFormSubmit);
 }
 
-},{"./sockets":6}],3:[function(require,module,exports){
+},{"./sockets":7}],3:[function(require,module,exports){
 "use strict";
 
 require("./login");
@@ -92,7 +92,7 @@ require("./chat");
 
 require("./paint");
 
-},{"./chat":1,"./login":2,"./paint":5,"./sockets":6}],4:[function(require,module,exports){
+},{"./chat":1,"./login":2,"./paint":5,"./sockets":7}],4:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -277,7 +277,22 @@ var handleFilled = function handleFilled(_ref3) {
 
 exports.handleFilled = handleFilled;
 
-},{"./sockets":6}],6:[function(require,module,exports){
+},{"./sockets":7}],6:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.handlePlayerUpdate = void 0;
+
+var handlePlayerUpdate = function handlePlayerUpdate(_ref) {
+  var sockets = _ref.sockets;
+  return console.log(sockets);
+};
+
+exports.handlePlayerUpdate = handlePlayerUpdate;
+
+},{}],7:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -290,6 +305,8 @@ var _notifications = require("./notifications");
 var _chat = require("./chat");
 
 var _paint = require("./paint");
+
+var _players = require("./players");
 
 var socket = null;
 
@@ -309,8 +326,9 @@ var initSockets = function initSockets(aSocket) {
   socket.on(events.beganPath, _paint.handleBeganPath);
   socket.on(events.strokedPath, _paint.handleStrokedPath);
   socket.on(events.filled, _paint.handleFilled);
+  socket.on(events.playerUpdate, _players.handlePlayerUpdate);
 };
 
 exports.initSockets = initSockets;
 
-},{"./chat":1,"./notifications":4,"./paint":5}]},{},[3]);
+},{"./chat":1,"./notifications":4,"./paint":5,"./players":6}]},{},[3]);
